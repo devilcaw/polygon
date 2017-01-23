0ffe01f1f87aff9ac58cba2096504f54efa6eda4
devilcaw < >
2017-01-23 20:30:35 +0700

    patch_car_more_stoping

--- Assets/car/scripts/car_control.cs
+++ Assets/car/scripts/car_control.cs
@@ -13,6 +13,8 @@ public class car_control : MonoBehaviour {
 	private float vAxis;
 	private float hAxis;
 	private bool brakeButton;
+	private	Vector3 frame_this;
+	private Vector3 frame_early;
 
 	[Header("Speed")]
 	public float speedMax;
@@ -22,10 +24,13 @@ public class car_control : MonoBehaviour {
 	void Start () {
 		GetComponent<Rigidbody> ().centerOfMass = centerOfMass.localPosition;
 		Physics.gravity = new Vector3 (0f, -29.4f, 0f);
+		frame_early = transform.position;
 	}
 	
 
 	void FixedUpdate () {
+
+		frame_this = transform.position;
 
 		if (Ccont.can_ride == true) {
 			vAxis = Input.GetAxis ("Vertical");
@@ -36,35 +41,41 @@ public class car_control : MonoBehaviour {
 			hAxis = 0;
 			brakeButton = true;
 		}
-			backCols [0].motorTorque = -vAxis * speedMax;
-			backCols [1].motorTorque = -vAxis * speedMax;
-			frontCols [0].motorTorque = -vAxis * speedMax;
-			frontCols [1].motorTorque = -vAxis * speedMax;
+		backCols [0].motorTorque = -vAxis * speedMax;
+		backCols [1].motorTorque = -vAxis * speedMax;
+		frontCols [0].motorTorque = -vAxis * speedMax;
+		frontCols [1].motorTorque = -vAxis * speedMax;
 
-			if (brakeButton) {
-				backCols [0].brakeTorque = speedBreak;
-				backCols [1].brakeTorque = speedBreak;
-				frontCols [0].brakeTorque = speedBreak;
-				frontCols [1].brakeTorque = speedBreak;
+		if (brakeButton == true) {
+			backCols [0].brakeTorque = speedBreak;
+			backCols [1].brakeTorque = speedBreak;
+			frontCols [0].brakeTorque = speedBreak;
+			frontCols [1].brakeTorque = speedBreak;
+			if (frame_this != frame_early)
+				GetComponent<Rigidbody> ().drag = 1;
+
 		
-			} else {
-				backCols [0].brakeTorque = 0;
-				backCols [1].brakeTorque = 0;
-				frontCols [0].brakeTorque = 0;
-				frontCols [1].brakeTorque = 0;
+		} else {
+			backCols [0].brakeTorque = 0;
+			backCols [1].brakeTorque = 0;
+			frontCols [0].brakeTorque = 0;
+			frontCols [1].brakeTorque = 0;
+			GetComponent<Rigidbody> ().drag = 0;
+		}
 
-			}
+		if (frame_this != frame_early)
+			frame_early = frame_this;
 
-			frontCols [0].steerAngle = hAxis * speedSide;
-			frontCols [1].steerAngle = hAxis * speedSide;
+		frontCols [0].steerAngle = hAxis * speedSide;
+		frontCols [1].steerAngle = hAxis * speedSide;
 
-			dataFront [0].Rotate (0, 0, -frontCols [0].rpm * Time.deltaTime);
-			dataFront [1].Rotate (0, 0, -frontCols [1].rpm * Time.deltaTime);
-			dataBack [0].Rotate (0, 0, -backCols [0].rpm * Time.deltaTime);
-			dataBack [1].Rotate (0, 0, -backCols [1].rpm * Time.deltaTime);
+		dataFront [0].Rotate (0, 0, -frontCols [0].rpm * Time.deltaTime);
+		dataFront [1].Rotate (0, 0, -frontCols [1].rpm * Time.deltaTime);
+		dataBack [0].Rotate (0, 0, -backCols [0].rpm * Time.deltaTime);
+		dataBack [1].Rotate (0, 0, -backCols [1].rpm * Time.deltaTime);
 
-			dataFront [0].localEulerAngles = new Vector3 (dataFront[0].localEulerAngles.x, hAxis * speedSide - 90, dataFront [0].localEulerAngles.z);
-			dataFront [1].localEulerAngles = new Vector3 (dataFront [1].localEulerAngles.x, hAxis * speedSide - 90, dataFront [1].localEulerAngles.z);
+		dataFront [0].localEulerAngles = new Vector3 (dataFront [0].localEulerAngles.x, hAxis * speedSide - 90, dataFront [0].localEulerAngles.z);
+		dataFront [1].localEulerAngles = new Vector3 (dataFront [1].localEulerAngles.x, hAxis * speedSide - 90, dataFront [1].localEulerAngles.z);
 
 	}
 }
