c1464dbf75b4b93e1a5b0e300ef85f663dd9fa78
devilcaw < >
2017-02-12 17:31:19 +0700

    patch

--- Assets/other/scripts/NewBehaviourScript.cs
+++ Assets/other/scripts/NewBehaviourScript.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class NewBehaviourScript : MonoBehaviour {
 	public Transform[] vertex_tr;
 	public List<int> arr = new List<int>();
+	private int j = 1;
+	public int[] trinagles = new int[100];
 
 	public Mesh Quad() {
 		//var normal = Vector3.Cross().normalized;
@@ -14,20 +16,21 @@ public class NewBehaviourScript : MonoBehaviour {
 			uvs [i] = new Vector2 (vertex_tr [i].localPosition.x, vertex_tr [i].localPosition.z);
 		}
 
-		for (int i = 2; i < vertex_tr.Length - 1; i++) {
-			for (int j = 1; j < 2; j++) {
-				if (j == 1) {
-					arr.Add (i);
-					arr.Add (i - 2);
-					arr.Add (i - 1);
-				} else {
-					arr.Add (i);
-					arr.Add (i - 1);
-					arr.Add (i + 1);
-				}
+		if (arr.Count < vertex_tr.Length)
+		for (int i = 2; i < vertex_tr.Length; i++) {
+			if (j == 1) {
+				arr.Add (i);
+				arr.Add (i - 2);
+				arr.Add (i - 1);
+				j = 2;
+			} else {
+				arr.Add (i);
+				arr.Add (i - 1);
+				arr.Add (i - 2);
+				j = 1;
 			}
 		}
-		Debug.Log (arr[1]);
+				
 
 		var mesh = new Mesh {
 			vertices = new[] {
@@ -48,13 +51,15 @@ public class NewBehaviourScript : MonoBehaviour {
 			},
 			uv = uvs,
 			//uv = new[] { new Vector2 (0, 0), new Vector2 (1, 0), new Vector2 (0, 1), new Vector2 (1, 1), new Vector2(0,0), new Vector2(0, 1) },
-			triangles = new[] { 0, 1, 2,
+			/*triangles = new[] { 0, 1, 2,
 				1, 3, 2, 
 				2, 3, 4,
 				3, 5, 4
-			}
+			}*/
+			triangles = trinagles
 		};
 		return mesh;
+
 	}
 	void Update() {
 		GetComponent<MeshFilter> ().mesh.Clear ();
@@ -62,5 +67,7 @@ public class NewBehaviourScript : MonoBehaviour {
 		GetComponent<MeshFilter> ().mesh.triangles = Quad ().triangles;
 		GetComponent<MeshFilter> ().mesh.uv = Quad ().uv;
 		GetComponent<MeshFilter> ().mesh.RecalculateNormals ();
+
+		arr.CopyTo (trinagles);
 	}
 }
